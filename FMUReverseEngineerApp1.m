12de889e5ab768f9a8162ce7b1b24b9b9fc78a10
zgrbykdr <bayrakdarozgur@gmail.com>
2026-04-02 11:20:39 +0200

    Add FMUReverseEngineerApp1 compatibility class wrapper

---
+++ FMUReverseEngineerApp1.m
@@ -0,0 +1,11 @@
+classdef FMUReverseEngineerApp1 < FMUReverseEngineerApp
+    % Compatibility alias class so users can instantiate the app as:
+    %   >> FMUReverseEngineerApp1
+    % This class intentionally delegates behavior to FMUReverseEngineerApp.
+
+    methods
+        function app = FMUReverseEngineerApp1
+            app@FMUReverseEngineerApp();
+        end
+    end
+end
